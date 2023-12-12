db80d7f0de8caefdba395fa93b14b91b08edff97
cold_hailstone <cksn1993@gmail.com>
2023-12-12 19:14:10 +0900

    4.14 Card Project part Three

--- App.js
+++ App.js
@@ -18,6 +18,22 @@ const Card = styled(Animated.createAnimatedComponent(View))`
     align-items: center;
     border-radius: 12px;
     box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
+    position: absolute;
+`;
+
+const Btn = styled.TouchableOpacity`
+    margin: 0px 10px;
+`;
+
+const BtnContainer = styled.View`
+    flex-direction: row;
+    flex: 1;
+`;
+
+const CardContainer = styled.View`
+    flex: 3;
+    justify-content: center;
+    align-items: center;
 `;
 
 export default function App() {
@@ -28,9 +44,16 @@ export default function App() {
         outputRange: ['-15deg', '15deg'],
         extrapolate: 'clamp',
     });
+    const secondSacle = position.interpolate({
+        inputRange: [-300, 0, 300],
+        outputRange: [1, 0.7, 1],
+        extrapolate: 'clamp',
+    });
     const onPressIn = Animated.spring(scale, { toValue: 0.95, useNativeDriver: true });
     const onPressOut = Animated.spring(scale, { toValue: 1, useNativeDriver: true });
     const goCenter = Animated.spring(position, { toValue: 0, useNativeDriver: true });
+    const goLeft = Animated.spring(position, { toValue: -500, tension: 5, useNativeDriver: true });
+    const goRight = Animated.spring(position, { toValue: 500, tension: 5, useNativeDriver: true });
     const panResponder = useRef(
         PanResponder.create({
             onStartShouldSetPanResponder: () => true,
@@ -39,27 +62,46 @@ export default function App() {
             },
             onPanResponderGrant: () => onPressIn.start(),
             onPanResponderRelease: (_, { dx }) => {
-                if (dx < -320) {
-                    Animated.spring(position, { toValue: -500, useNativeDriver: true }).start();
-                } else if (dx > 320) {
-                    Animated.spring(position, { toValue: 500, useNativeDriver: true }).start();
+                if (dx < -250) {
+                    goLeft.start();
+                } else if (dx > 250) {
+                    goRight.start();
                 } else {
                     Animated.parallel([onPressOut, goCenter]).start();
                 }
             },
         })
     ).current;
+    const closePress = () => {
+        goLeft.start();
+    };
+    const checkmarkPress = () => {
+        goRight.start();
+    };
 
     return (
         <Container>
-            <Card
-                {...panResponder.panHandlers}
-                style={{
-                    transform: [{ scale }, { translateX: position }, { rotateZ: rotation }],
-                }}
-            >
-                <Ionicons name='pizza' color='#192a56' size={98} />
-            </Card>
+            <CardContainer>
+                <Card style={{ transform: [{ scale: secondSacle }] }}>
+                    <Ionicons name='beer' color='#192a56' size={98} />
+                </Card>
+                <Card
+                    {...panResponder.panHandlers}
+                    style={{
+                        transform: [{ scale }, { translateX: position }, { rotateZ: rotation }],
+                    }}
+                >
+                    <Ionicons name='pizza' color='#192a56' size={98} />
+                </Card>
+            </CardContainer>
+            <BtnContainer>
+                <Btn onPress={closePress}>
+                    <Ionicons name='close-circle' color='white' size={42} />
+                </Btn>
+                <Btn onPress={checkmarkPress}>
+                    <Ionicons name='checkmark-circle' color='white' size={42} />
+                </Btn>
+            </BtnContainer>
         </Container>
     );
 }
