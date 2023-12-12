be49f989abd452750c443944f420cf86e509a05f
cold_hailstone <cksn1993@gmail.com>
2023-12-12 14:30:50 +0900

    4.9 Pan Responder

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from 'react';
-import { Animated, Dimensions, Easing, Pressable } from 'react-native';
+import React, { useRef } from 'react';
+import { Animated, PanResponder } from 'react-native';
 import styled from 'styled-components/native';
 
 const Container = styled.View`
@@ -14,47 +14,13 @@ const Box = styled.View`
 `;
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
-
 export default function App() {
     const position = useRef(
-        new Animated.ValueXY({ x: -SCREEN_WIDTH / 2 + 100, y: -SCREEN_HEIGHT / 2 + 100 })
+        new Animated.ValueXY({
+            x: 0,
+            y: 0,
+        })
     ).current;
-    const topLeft = Animated.timing(position, {
-        toValue: {
-            x: -SCREEN_WIDTH / 2 + 100,
-            y: -SCREEN_HEIGHT / 2 + 100,
-        },
-        useNativeDriver: false,
-    });
-    const topRight = Animated.timing(position, {
-        toValue: {
-            x: SCREEN_WIDTH / 2 - 100,
-            y: -SCREEN_HEIGHT / 2 + 100,
-        },
-        useNativeDriver: false,
-    });
-    const bottomLeft = Animated.timing(position, {
-        toValue: {
-            x: -SCREEN_WIDTH / 2 + 100,
-            y: SCREEN_HEIGHT / 2 - 100,
-        },
-        useNativeDriver: false,
-    });
-    const bottomRight = Animated.timing(position, {
-        toValue: {
-            x: SCREEN_WIDTH / 2 - 100,
-            y: SCREEN_HEIGHT / 2 - 100,
-        },
-        useNativeDriver: false,
-    });
-    const moveUp = () => {
-        Animated.loop(Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])).start();
-    };
-    const rotation = position.y.interpolate({
-        inputRange: [-300, 300],
-        outputRange: ['-360deg', '360deg'],
-    });
     const borderRadius = position.y.interpolate({
         inputRange: [-300, 300],
         outputRange: [100, 0],
@@ -63,18 +29,28 @@ export default function App() {
         inputRange: [-300, 300],
         outputRange: ['rgb(255, 99, 71)', 'rgb(71, 166, 255)'],
     });
+    const panResponder = useRef(
+        PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (_, { dx, dy }) => {
+                position.setValue({
+                    x: dx,
+                    y: dy,
+                });
+            },
+        })
+    ).current;
 
     return (
         <Container>
-            <Pressable onPress={moveUp}>
-                <AnimatedBox
-                    style={{
-                        borderRadius,
-                        backgroundColor: bgColor,
-                        transform: [...position.getTranslateTransform()],
-                    }}
-                />
-            </Pressable>
+            <AnimatedBox
+                {...panResponder.panHandlers}
+                style={{
+                    borderRadius,
+                    backgroundColor: bgColor,
+                    transform: [...position.getTranslateTransform()],
+                }}
+            />
         </Container>
     );
 }
